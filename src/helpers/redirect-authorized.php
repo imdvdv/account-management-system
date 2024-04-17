30c8f9ef3604956316f1ce3b0b54895663baefea
Igor Medvedev <vinnepooh55@gmail.com>
2024-04-17 20:06:32 +0600

    corrected the path to the includes file

--- src/helpers/redirect-authorized.php
+++ src/helpers/redirect-authorized.php
@@ -1,6 +1,6 @@
 <?php
 
-include_once "{$_SERVER["DOCUMENT_ROOT"]}/src/configs/includes.php";
+include_once __DIR__ . "/../config/includes.php";
 
 function redirectAuthorized (): void {
     if (isAuthorized()){
