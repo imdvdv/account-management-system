52c7e6579a5a3e7d938907791488126f2d455b18
Igor Medvedev <vinnepooh55@gmail.com>
2024-05-09 23:11:53 +0600

    included remove-extra-function file

--- src/config/includes.php
+++ src/config/includes.php
@@ -5,6 +5,7 @@ include_once __DIR__ . "/env.php";
 
 // INCLUDE FUNCTIONS FOR AUTHORIZATION CHECK
 include_once __DIR__ . "/../helpers/db-connection.php";
+include_once __DIR__ . "/../helpers/remove-extra-spaces.php";
 include_once __DIR__ . "/../helpers/validation.php";
 include_once __DIR__ . "/../helpers/get-random-code-data.php";
 include_once __DIR__ . "/../helpers/auth-by-token.php";
